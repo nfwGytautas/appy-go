dc38a1b925b3b23310a11f0a27f15bab10eb5fbe
Gytautas Kazlauskas <gytautas.kk@gmail.com>
2024-03-24 23:28:22 +0000

    current function name

--- utils/reflect.go
+++ utils/reflect.go
@@ -8,3 +8,11 @@ import (
 func ReflectFunctionName(fn interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
 }
+
+func ReflectCurrentFunction() string {
+	pc := make([]uintptr, 15)
+	n := runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	frame, _ := frames.Next()
+	return frame.Function
+}
