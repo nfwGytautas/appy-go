94ced63417595a50122fbeef8faac8eb5e29fb22
nfwGytautas <gytautas.kk@gmail.com>
2024-10-21 11:32:13 +0000

    eventbus

---
+++ eventbus.go
@@ -0,0 +1,33 @@
+package appy
+
+import "sync"
+
+type EventFunc[T any] func(T)
+
+type EventChannel[T any] struct {
+	mtx         sync.Mutex
+	subscribers []EventFunc[T]
+}
+
+func NewEventChannel[T any]() *EventChannel[T] {
+	return &EventChannel[T]{
+		mtx: sync.Mutex{},
+	}
+}
+
+func (ec *EventChannel[T]) Subscribe(subscriber EventFunc[T]) {
+	ec.subscribers = append(ec.subscribers, subscriber)
+}
+
+func (ec *EventChannel[T]) Publish(event T) {
+	ec.mtx.Lock()
+	defer ec.mtx.Unlock()
+
+	for _, subscriber := range ec.subscribers {
+		subscriber(event)
+	}
+}
+
+func (ec *EventChannel[T]) PublishAsync(event T) {
+	go ec.Publish(event)
+}
